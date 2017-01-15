1ab6934f7e0f8c0dce85009c32de31176b67be71
ProbableCohort <brenthaertlein@gmail.com>
2017-01-15 15:25:09 -0500

    Refactor $stateChangeError handler to use a switch statement

--- public/javascripts/config/states/main.js
+++ public/javascripts/config/states/main.js
@@ -21,14 +21,20 @@
     })
     .run(($rootScope, $state) => {
       $rootScope.$on('$stateChangeError', (evt, to, toParams, from, fromParams, error) => {
-        console.log(error);
-        if (error.code === 'NO_USER_FOUND') {
-          $state.go('login')
-        } else if (error.redirectTo) {
-          console.log(`Redirect to ${error.redirectTo} because ${error.code}`)
-          $state.transitionTo(error.redirectTo, {
-            reason: error.code
-          });
+        switch (error.code) {
+          case 'NO_USER_FOUND':
+            $state.go('login');
+            break;
+          case 'PERMISSIONS_INVALID':
+            console.log(`Redirect to ${error.redirectTo} because ${error.code}`)
+            $state.transitionTo(error.redirectTo, {
+              reason: error.code
+            });
+            break;
+          default:
+            console.error('No error code found, redirecting home', error);
+            $state.go('home');
+            break;
         }
       })
     })
